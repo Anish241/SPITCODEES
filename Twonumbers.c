78b96d140451f642b8583798fe6ffd51bfe6e009
Anish241 <88095140+Anish241@users.noreply.github.com>
2022-03-08 20:48:09 +0530

    Create Twonumbers.c

---
+++ Twonumbers.c
@@ -0,0 +1,38 @@
+
+#include<stdio.h>
+int main()
+{ int no1 , no2;
+  printf("Number 1: ");
+  scanf("%d",&no1);
+  printf("Number2: ");
+  scanf("%d",&no2);
+   int add= no1+no2;
+   int sub= no1-no2;
+   int mul=no1*no2;
+   int div =no1/no2;
+   int mod= no1%no2;
+  printf("The addition of two number is %d \n",add);
+   printf("The subtraction of two number is %d \n",sub);
+   printf("The multiplication of two numbers is %d\n",mul);
+   printf("The division of two number is %d \n", div);
+   printf("The modulus of two numbers is %d\n", mod);
+
+  
+  return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
